6965f40e6bc7d4b4eef2279167bac22153b4bbbd
Antonio Tomas Lorente Mur <antonio-tomas.lorente-mur@insa-lyon.fr>
2021-01-12 20:12:21 +0100

    Fixed some bugs in the setup file

--- setup.py
+++ setup.py
@@ -1,24 +1,55 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='spyrit',
-      version='0.1',
-      description='Single-Pixel Image Reconstruction Toolbox for Python ',
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(name='spyritest',
+      version='1.0.0',
+      description='Demo package',
       url='https://gitlab.in2p3.fr/antonio-tomas.lorente-mur/spyrit',
+      long_description = readme(),
+      long_description_content_type = "text/markdown",
+      classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+      ],
       author='Antonio Tomas Lorente Mur, Nicolas Ducros, Sebastien Crombez',
       author_email='Nicolas.Ducros@insa-lyon.fr',
+      keywords = "tutorial package",
       license='Attribution-ShareAlike 4.0 International',
       install_requires=[
-          'numpy (>1.3.0)',
-          'matplotlib (>2.2.4)',
-          'scipy (>1.1.0)',
-          'torch (>1.1.0)',
-          'torchvision (>0.2.2)',
-          'PIL (>5.3.0)',
-          'opencv-python (>4.0.0)',
-          'imutils (>0.5.3)',
-          'PyWavelets (>1.0.1)',
-          'imageio (>2.9.0)',
+          'numpy (==1.19.3)',
+          'matplotlib (==3.3.3)',
+          'scipy (==1.5.2)',
+          'torch (==1.7.1)',
+          'torchvision (==0.8.2)',
+          'Pillow (==7.2.0)',
+          'opencv-python (==4.5.1.48)',
+          'imutils (==0.5.3)',
+          'PyWavelets',
+          'imageio (==2.9.0)',
+          'fht (==1.0.2)',
       ],
-      dependency_links=['https://github.com/nbarbey/fht'],
-      packages=['spyrit'],
+      packages=find_packages(),
       zip_safe=False)
+
+
+#
+
+#
+#      install_requires=[
+#          'numpy',
+#          'matplotlib',
+#          'scipy',
+#          'torch',
+#          'torchvision',
+#          'Pillow',
+#          'opencv-python',
+#          'imutils',
+#          'PyWavelets',
+#          'imageio',
+#          'fht',
+# 
