729bc9add7891e60e4f82ab13646b26fe7b3ea14
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2021-02-01 11:06:59 +0100

    Remove version of dependencies
    Keep version for numpy because the wheel is compiled with that version.
    Keep version for torch and torchvision to be able to reproduce the code

--- .github/workflows/actionsCreateWheelLinux.sh
+++ .github/workflows/actionsCreateWheelLinux.sh
@@ -4,7 +4,7 @@ set -e -x
 echo ${PYTHONFOLDER}
 mkdir /software
 cd /home/
-/opt/python/${PYTHONFOLDER}/bin/pip install numpy
+/opt/python/${PYTHONFOLDER}/bin/pip install numpy==1.19.3
 /opt/python/${PYTHONFOLDER}/bin/python setup.py sdist bdist_wheel
 auditwheel repair /home/dist/*.whl -w /software/wheelhouse/ --plat "manylinux2014_x86_64"
 cp -r /software/wheelhouse /home/
