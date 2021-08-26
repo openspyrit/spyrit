ef1f7ef8390c7e91c2f031ee4237121c17460245
cmouton <claire.mouton@creatis.insa-lyon.fr>
2021-08-26 13:10:59 +0200

    Improved conf.py.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,7 +72,7 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = 'sphinx_rtd_theme'
 
 # on_rtd is whether we are on readthedocs.org
