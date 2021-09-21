60b9381cfbd566b79244950ce221c2523f5b87fd
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2021-09-21 10:09:14 +0200

    Towards 1.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
         return f.read()
 
 setup(name='spyrit',
-      version='1.0.0',
+      version='1.1.0',
       description='Demo package',
       url='https://github.com/openspyrit/spyrit',
       long_description = readme(),
