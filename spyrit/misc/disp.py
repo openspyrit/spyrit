a5d528721a8e65ec4459bab30b04f70810598abf
Nicolas Ducros <nicolas.ducros@insa-lyon.fr>
2022-09-14 19:55:00 +0200

    add colorbar and noaxis functions

--- spyrit/misc/disp.py
+++ spyrit/misc/disp.py
@@ -191,6 +191,29 @@ def plot(x,y,title='',xlabel='', ylabel='', color='black'):
     plt.ylabel(ylabel);
     plt.show();
     
+def add_colorbar(mappable):
+    """
+    Example: 
+        f, axs = plt.subplots(1, 2)
+        im = axs[0].imshow(img1, cmap='gray') 
+        add_colorbar(im)
+        im = axs[0].imshow(img2, cmap='gray') 
+        add_colorbar(im)
+    """
+    last_axes = plt.gca()
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = fig.colorbar(mappable, cax=cax)
+    plt.sca(last_axes)
+    return cbar
+
+def noaxis(axs):
+    for ax in axs:
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    
 def string_mean_std(x, prec = 3):
     return "{:.{p}f} +/- {:.{p}f}".format(np.mean(x), np.std(x), p=prec)
     
