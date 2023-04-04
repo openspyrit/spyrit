79cbf8ac5822d89354acedfac7df73bec8d01c64
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2023-04-04 10:46:05 +0200

    Add assert into test Modify HadamSplit

--- spyrit/test/run_tests.py
+++ spyrit/test/run_tests.py
@@ -1,8 +1,14 @@
 
 from test_core_meas import test_core_meas
+from test_core_noise import test_core_noise
+from test_core_prep import test_core_prep
+from test_core_recon import test_core_recon
 
 def run_tests():
     test_core_meas()
+    test_core_noise()
+    test_core_prep()
+    test_core_recon()
 
 if __name__ == '__main__':
     run_tests()
