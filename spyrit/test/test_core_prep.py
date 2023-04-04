493a700e80cd244a005a336d943d09f1edf26e50
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2023-04-04 10:46:05 +0200

    Add assert into test Modify HadamSplit

--- spyrit/test/test_core_prep.py
+++ spyrit/test/test_core_prep.py
@@ -9,92 +9,108 @@ from spyrit.core.meas import Linear
 from spyrit.core.prep import DirectPoisson
 import numpy as np
 import torch
+from test_helpers import assert_test
 
-# constructor and forward
-x = torch.rand([10,400], dtype=torch.float)
-H = np.random.random([400,32*32])
-meas_op =  Linear(H)
-prep_op = DirectPoisson(1.0, meas_op)
-m = prep_op(x)
-print(m.shape)
+def test_core_prep():
+    # constructor and forward
+    x = torch.rand([10,400], dtype=torch.float)
+    H = np.random.random([400,32*32])
+    meas_op =  Linear(H)
+    prep_op = DirectPoisson(1.0, meas_op)
+    m = prep_op(x)
+    print(m.shape)
+    assert_test(m.shape, torch.Size([10, 400]), "Wrong matrix size")
 
-# variance
-x = torch.rand([10,400], dtype=torch.float)
-v = prep_op.sigma(x)
-print(v.shape)
+    # variance
+    x = torch.rand([10,400], dtype=torch.float)
+    v = prep_op.sigma(x)
+    print(v.shape)
+    assert_test(v.shape, torch.Size([10, 400]), "Wrong matrix size")
 
-# denormalize_expe
-x = torch.rand([10, 1, 32,32], dtype=torch.float)
-beta = 9*torch.rand([10])
-y = prep_op.denormalize_expe(x, beta, 32, 32)
-print(y.shape)
+    # denormalize_expe
+    x = torch.rand([10, 1, 32,32], dtype=torch.float)
+    beta = 9*torch.rand([10])
+    y = prep_op.denormalize_expe(x, beta, 32, 32)
+    print(y.shape)
+    assert_test(y.shape, torch.Size([10, 1, 32, 32]), "Wrong matrix size")
 
-#%% Test SplitPoisson
-import torch
-import numpy as np
-from spyrit.core.meas import LinearSplit, HadamSplit
-from spyrit.core.prep import SplitPoisson
+    #%% Test SplitPoisson
+    from spyrit.core.meas import LinearSplit, HadamSplit
+    from spyrit.core.prep import SplitPoisson
 
-# constructor
-split_op = SplitPoisson(10, 400, 32*32)
+    # constructor
+    split_op = SplitPoisson(10, 400, 32*32)
 
-# forward with LinearSplit
-x = torch.rand([10,2*400], dtype=torch.float)
-H = np.random.random([400,32*32])
+    # forward with LinearSplit
+    x = torch.rand([10,2*400], dtype=torch.float)
+    H = np.random.random([400,32*32])
 
-# forward
-meas_op =  LinearSplit(H)
-m = split_op(x, meas_op)
-print(m.shape)
+    # forward
+    meas_op =  LinearSplit(H)
+    m = split_op(x, meas_op)
+    print(m.shape)
+    assert_test(m.shape, torch.Size([10, 400]), "Wrong matrix size")
 
-# forward with HadamSplit
-Perm = np.random.random([32*32,32*32])
-meas_op = HadamSplit(H, Perm, 32, 32)
-m = split_op(x, meas_op)
-print(m.shape)
+    # forward with HadamSplit
+    Perm = np.random.random([32,32])
+    meas_op = HadamSplit(400, 32,  Perm)
+    m = split_op(x, meas_op)
+    print(m.shape)
+    assert_test(m.shape, torch.Size([10, 400]), "Wrong matrix size")
 
-# forward_expe
-m, alpha = split_op.forward_expe(x, meas_op)
-print(m.shape)
-print(alpha.shape)
+    # forward_expe
+    m, alpha = split_op.forward_expe(x, meas_op)
+    print(m.shape)
+    assert_test(m.shape, torch.Size([10, 400]), "Wrong matrix size")
+    print(alpha.shape)
+    assert_test(alpha.shape, torch.Size([10]), "Wrong matrix size")
 
-# sigma
-x = torch.rand([10,2*400], dtype=torch.float)
-v = split_op.sigma(x)
-print(v.shape)
+    # sigma
+    x = torch.rand([10,2*400], dtype=torch.float)
+    v = split_op.sigma(x)
+    print(v.shape)
+    assert_test(v.shape, torch.Size([10, 400]), "Wrong matrix size")
 
-# set_expe
-split_op.set_expe(gain=1.6)
-print(split_op.gain)
+    # set_expe
+    split_op.set_expe(gain=1.6)
+    print(split_op.gain)
+    assert_test(split_op.gain, 1.6, "Wrong gain")
 
-# sigma_expe
-v = split_op.sigma_expe(x)
-print(v.shape)
+    # sigma_expe
+    v = split_op.sigma_expe(x)
+    print(v.shape)
+    assert_test(v.shape, torch.Size([10, 400]), "Wrong matrix size")
 
-# sigma_from_image
-x = torch.rand([10,32*32], dtype=torch.float)
-v = split_op.sigma_from_image(x, meas_op)
-print(v.shape)
+    # sigma_from_image
+    x = torch.rand([10,32*32], dtype=torch.float)
+    v = split_op.sigma_from_image(x, meas_op)
+    print(v.shape)
+    assert_test(v.shape, torch.Size([10, 400]), "Wrong matrix size")
 
-# denormalize_expe
-x = torch.rand([10, 1, 32,32], dtype=torch.float)
-beta = 9*torch.rand([10,1])
-y = split_op.denormalize_expe(x, beta, 32, 32)
-print(y.shape)
+    # denormalize_expe
+    x = torch.rand([10, 1, 32,32], dtype=torch.float)
+    beta = 9*torch.rand([10,1])
+    y = split_op.denormalize_expe(x, beta, 32, 32)
+    print(y.shape)
+    assert_test(y.shape, torch.Size([10, 1, 32, 32]), "Wrong matrix size")
 
-#%% Test SplitRowPoisson
-from spyrit.core.meas import LinearRowSplit
-from spyrit.core.prep import SplitRowPoisson
+    #%% Test SplitRowPoisson
+    from spyrit.core.meas import LinearRowSplit
+    from spyrit.core.prep import SplitRowPoisson
 
-# constructor
-split_op = SplitRowPoisson(2.0, 24, 64)
+    # constructor
+    split_op = SplitRowPoisson(2.0, 24, 64)
 
-# forward with LinearRowSplit
-x = torch.rand([10,48,64], dtype=torch.float)
-H_pos = np.random.random([24,64])
-H_neg = np.random.random([24,64])
-meas_op = LinearRowSplit(H_pos, H_neg)
+    # forward with LinearRowSplit
+    x = torch.rand([10,48,64], dtype=torch.float)
+    H_pos = np.random.random([24,64])
+    H_neg = np.random.random([24,64])
+    meas_op = LinearRowSplit(H_pos, H_neg)
 
-# forward
-m = split_op(x, meas_op)
-print(m.shape)
+    # forward
+    m = split_op(x, meas_op)
+    print(m.shape)
+    assert_test(m.shape, torch.Size([10, 24, 64]), "Wrong matrix size")
+
+if __name__ == '__main__':
+    test_core_prep()
--- spyrit/test/test_core_recon.py
+++ spyrit/test/test_core_recon.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 #%% PseudoInverse
 import numpy as np
 import torch
@@ -8,217 +7,223 @@ from spyrit.core.meas import HadamSplit
 from spyrit.core.noise import NoNoise
 from spyrit.core.prep import SplitPoisson 
 from spyrit.core.recon import PseudoInverse
+from test_helpers import assert_test
 
-# EXAMPLE 1
-# constructor
-recon_op = PseudoInverse()
+def test_core_recon():
+    # EXAMPLE 1
+    # constructor
+    recon_op = PseudoInverse()
 
-# forward
-Ord = np.random.random([32,32])
-meas_op = HadamSplit(400, 32, Ord)
-y = torch.rand([85,400], dtype=torch.float)  
+    # forward
+    Ord = np.random.random([32,32])
+    meas_op = HadamSplit(400, 32, Ord)
+    y = torch.rand([85,400], dtype=torch.float)  
 
-x = recon_op(y, meas_op)
-print(x.shape)
+    x = recon_op(y, meas_op)
+    print(x.shape)
+    assert_test(x.shape, torch.Size([85, 1024]), "Wrong forward size")
 
-# EXAMPLE 2
-# constructor
-M = 64
-H = 64
-B = 1
+    # EXAMPLE 2
+    # constructor
+    M = 64
+    H = 64
+    B = 1
 
-Ord = np.random.random([H,H])
-meas_op = HadamSplit(M, H, Ord)
-noise_op = NoNoise(meas_op)
-split_op = SplitPoisson(1.0, M, H**2)
-recon_op = PseudoInverse()
+    Ord = np.random.random([H,H])
+    meas_op = HadamSplit(M, H, Ord)
+    noise_op = NoNoise(meas_op)
+    split_op = SplitPoisson(1.0, M, H**2)
+    recon_op = PseudoInverse()
 
-x = torch.FloatTensor(B,H**2).uniform_(-1, 1)
-y = noise_op(x)
-m = split_op(y, meas_op)
-z = recon_op(m, meas_op)
-print(z.shape)
-print(torch.linalg.norm(x - z)/torch.linalg.norm(x))
+    x = torch.FloatTensor(B,H**2).uniform_(-1, 1)
+    y = noise_op(x)
+    m = split_op(y, meas_op)
+    z = recon_op(m, meas_op)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([1, 4096]), "Wrong recon size")
+    tensor_test = torch.linalg.norm(x - z)/torch.linalg.norm(x)
+    print(tensor_test)
+    #assert_test(tensor_test, 0.9902, "Wrong tensor value")
 
-#%% PseudoInverseStore
-from spyrit.core.meas import Linear
-#from spyrit.core.prep import DirectPoisson
-from spyrit.core.recon import PseudoInverseStore
-import numpy as np
-import torch
+    #%% PseudoInverseStore
+    from spyrit.core.meas import Linear
+    #from spyrit.core.prep import DirectPoisson
+    from spyrit.core.recon import PseudoInverseStore
 
-H = np.random.rand(400,32*32)
-meas_op = Linear(H)
-recon_op = PseudoInverseStore(meas_op)
-x = torch.rand([10,400], dtype=torch.float)
-y = recon_op(x)
-print(y.shape)
+    H = np.random.rand(400,32*32)
+    meas_op = Linear(H)
+    recon_op = PseudoInverseStore(meas_op)
+    x = torch.rand([10,400], dtype=torch.float)
+    y = recon_op(x)
+    print(y.shape)
+    assert_test(y.shape, torch.Size([10, 1024]), "Wrong recon size")
 
-#%% PseudoInverseStore2
-import numpy as np
-import torch
-from spyrit.core.meas import LinearRowSplit
-from spyrit.core.noise import NoNoise
-from spyrit.core.prep import SplitRowPoisson
-from spyrit.core.recon import PseudoInverseStore2
-from spyrit.misc.walsh_hadamard import walsh_matrix
-
-
-# EXAMPLE 1
-# constructor
-H_pos = np.random.rand(24,64)
-H_neg = np.random.rand(24,64)
-meas_op = LinearRowSplit(H_pos,H_neg)
-recon_op = PseudoInverseStore2(meas_op)
-
-# forward
-x = torch.rand([10,24,92], dtype=torch.float)
-y = recon_op(x)
-print(y.shape)
-
-# EXAMPLE 2
-# constructor
-M = 63
-N = 64
-B = 1
-
-H = walsh_matrix(N)
-H_pos = np.where(H>0,H,0)[:M,:]
-H_neg = np.where(H<0,-H,0)[:M,:]
-meas_op = LinearRowSplit(H_pos,H_neg)
-noise_op = NoNoise(meas_op)
-split_op = SplitRowPoisson(1.0, M, 92)
-recon_op = PseudoInverseStore2(meas_op)
-
-# forward
-x = torch.FloatTensor(B,N,92).uniform_(-1, 1)
-y = noise_op(x)
-m = split_op(y, meas_op)
-z = recon_op(m)
-print(z.shape)
-print(torch.linalg.norm(x - z)/torch.linalg.norm(x))
-
-#%% PinvNet
-import numpy as np
-import torch
-from spyrit.core.meas import HadamSplit
-from spyrit.core.noise import NoNoise
-from spyrit.core.prep import SplitPoisson 
-from spyrit.core.recon import PseudoInverse, PinvNet
-
-# EXAMPLE
-# constructor
-B, C, H, M = 10, 1, 64, 64**2
-Ord = np.ones((H,H))
-meas = HadamSplit(M, H, Ord)
-noise = NoNoise(meas)
-prep = SplitPoisson(1.0, M, H**2)
-recnet = PinvNet(noise, prep) 
-
-# forward
-x = torch.FloatTensor(B,C,H,H).uniform_(-1, 1)
-z = recnet(x)
-print(z.shape)
-print(torch.linalg.norm(x - z)/torch.linalg.norm(x))
-
-# meas2img
-x = torch.rand(B*C,2*M)
-z = recnet.meas2img(x)
-print(z.shape)
-
-# acquire
-x = torch.FloatTensor(B,C,H,H).uniform_(-1, 1)
-z = recnet.acquire(x)
-print(z.shape)
-
-# reconstruct
-x = torch.rand((B*C,2*M), dtype=torch.float)
-z = recnet.reconstruct(x)
-print(z.shape)
-
-#%% PinvStoreNet
-import numpy as np
-import torch
-from spyrit.core.meas import HadamSplit
-from spyrit.core.noise import Poisson
-from spyrit.core.prep import DirectPoisson 
-from spyrit.core.recon import PseudoInverseStore, PinvStoreNet
+    #%% PseudoInverseStore2
+    from spyrit.core.meas import LinearRowSplit
+    from spyrit.core.prep import SplitRowPoisson
+    from spyrit.core.recon import PseudoInverseStore2
+    from spyrit.misc.walsh_hadamard import walsh_matrix
 
 
-B, C, h, M = 85, 1, 32, 600
+    # EXAMPLE 1
+    # constructor
+    H_pos = np.random.rand(24,64)
+    H_neg = np.random.rand(24,64)
+    meas_op = LinearRowSplit(H_pos,H_neg)
+    recon_op = PseudoInverseStore2(meas_op)
 
-# constructor
-H = np.random.random([M, h**2])
-meas =  Linear(H)
-meas.h, meas.w = h, h
-noise = NoNoise(meas)
-prep = DirectPoisson(1.0, meas)
-pinv_net = PinvStoreNet(noise, prep)
+    # forward
+    x = torch.rand([10,24,92], dtype=torch.float)
+    y = recon_op(x)
+    print(y.shape)
+    assert_test(y.shape, torch.Size([10, 64, 92]), "Wrong recon size")
 
-# forward
-x = torch.FloatTensor(B,C,h,h).uniform_(-1, 1)
-z = pinv_net(x)
+    # EXAMPLE 2
+    # constructor
+    M = 63
+    N = 64
+    B = 1
 
-print(z.shape)
-print(torch.linalg.norm(x - z)/torch.linalg.norm(x))
+    H = walsh_matrix(N)
+    H_pos = np.where(H>0,H,0)[:M,:]
+    H_neg = np.where(H<0,-H,0)[:M,:]
+    meas_op = LinearRowSplit(H_pos,H_neg)
+    noise_op = NoNoise(meas_op)
+    split_op = SplitRowPoisson(1.0, M, 92)
+    recon_op = PseudoInverseStore2(meas_op)
 
-# acquire
-x = torch.FloatTensor(B,C,h,h).uniform_(-1, 1)
-z = pinv_net.acquire(x)
-print(z.shape)
+    # forward
+    x = torch.FloatTensor(B,N,92).uniform_(-1, 1)
+    y = noise_op(x)
+    m = split_op(y, meas_op)
+    z = recon_op(m)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([1, 64, 92]), "Wrong recon size")
+    tensor_test = torch.linalg.norm(x - z)/torch.linalg.norm(x)
+    print(tensor_test)
+    #assert_test(tensor_test, tensor(0.0989), "Wrong tensor value")
 
-# reconstruct
-x = torch.rand((B*C,M), dtype=torch.float)
-z = pinv_net.reconstruct(x)
-print(z.shape)
+    #%% PinvNet
+    from spyrit.core.recon import PinvNet
 
-#%% TikhonovMeasurementPriorDiag
-import numpy as np
-import torch
-from spyrit.core.recon import TikhonovMeasurementPriorDiag
-from spyrit.core.meas import HadamSplit
+    # EXAMPLE
+    # constructor
+    B, C, H, M = 10, 1, 64, 64**2
+    Ord = np.ones((H,H))
+    meas = HadamSplit(M, H, Ord)
+    noise = NoNoise(meas)
+    prep = SplitPoisson(1.0, M, H**2)
+    recnet = PinvNet(noise, prep) 
 
-B, H, M = 85, 32, 512
+    # forward
+    x = torch.FloatTensor(B,C,H,H).uniform_(-1, 1)
+    z = recnet(x)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([10, 1, 64, 64]), "Wrong recon size")
+    tensor_test = torch.linalg.norm(x - z)/torch.linalg.norm(x)
+    print(tensor_test)
+    #assert_test(tensor_test, tensor(5.8252e-06), "Wrong tensor value")
 
-# constructor
-sigma = np.random.random([H**2, H**2])
-recon = TikhonovMeasurementPriorDiag(sigma, M)
+    # meas2img
+    x = torch.rand(B*C,2*M)
+    z = recnet.meas2img(x)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([10, 1, 64, 64]), "Wrong recon size")
 
-# forward
-Ord = np.ones((H,H))
-meas = HadamSplit(M, H, Ord)
-y = torch.rand([B,M], dtype=torch.float)  
-x_0 = torch.zeros((B, H**2), dtype=torch.float)
-var = torch.zeros((B, M), dtype=torch.float)
+    # acquire
+    x = torch.FloatTensor(B,C,H,H).uniform_(-1, 1)
+    z = recnet.acquire(x)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([10, 8192]), "Wrong recon size")
 
-x = recon(y, x_0, var, meas)
-print(x.shape)
+    # reconstruct
+    x = torch.rand((B*C,2*M), dtype=torch.float)
+    z = recnet.reconstruct(x)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([10, 1, 64, 64]), "Wrong recon size")
 
-#%% DCNet
-import numpy as np
-import torch
-from spyrit.core.recon import DCNet
-from spyrit.core.meas import HadamSplit
-from spyrit.core.noise import NoNoise
-from spyrit.core.prep import SplitPoisson 
-from spyrit.core.meas import HadamSplit
+    #%% PinvStoreNet
+    from spyrit.core.noise import Poisson
+    from spyrit.core.prep import DirectPoisson 
+    from spyrit.core.recon import PseudoInverseStore, PinvStoreNet
 
-B, C, H, M = 10, 1, 64, 64**2//2
 
-# constructor
-Ord = np.ones((H,H))
-meas = HadamSplit(M, H, Ord)
-noise = NoNoise(meas)
-prep = SplitPoisson(1.0, M, H**2)
-sigma = np.random.random([H**2, H**2])
-recnet = DCNet(noise,prep,sigma)
+    B, C, h, M = 85, 1, 32, 600
 
-# forward
-x = torch.FloatTensor(B,C,H,H).uniform_(-1, 1)
-z = recnet(x)
-print(z.shape)
+    # constructor
+    H = np.random.random([M, h**2])
+    meas =  Linear(H)
+    meas.h, meas.w = h, h
+    noise = NoNoise(meas)
+    prep = DirectPoisson(1.0, meas)
+    pinv_net = PinvStoreNet(noise, prep)
 
-# reconstruct
-x = torch.rand((B*C,2*M), dtype=torch.float)
-z = recnet.reconstruct(x)
-print(z.shape)
+    # forward
+    x = torch.FloatTensor(B,C,h,h).uniform_(-1, 1)
+    z = pinv_net(x)
+
+    print(z.shape)
+    assert_test(z.shape, torch.Size([85, 1, 32, 32]), "Wrong recon size")
+    tensor_test = torch.linalg.norm(x - z)/torch.linalg.norm(x)
+    print(tensor_test)
+    #assert_test(tensor_test, tensor(0.6432), "Wrong tensor value")
+
+    # acquire
+    x = torch.FloatTensor(B,C,h,h).uniform_(-1, 1)
+    z = pinv_net.acquire(x)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([85, 600]), "Wrong recon size")
+
+    # reconstruct
+    x = torch.rand((B*C,M), dtype=torch.float)
+    z = pinv_net.reconstruct(x)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([85, 1, 32, 32]), "Wrong recon size")
+
+    #%% TikhonovMeasurementPriorDiag
+    from spyrit.core.recon import TikhonovMeasurementPriorDiag
+
+    B, H, M = 85, 32, 512
+
+    # constructor
+    sigma = np.random.random([H**2, H**2])
+    recon = TikhonovMeasurementPriorDiag(sigma, M)
+
+    # forward
+    Ord = np.ones((H,H))
+    meas = HadamSplit(M, H, Ord)
+    y = torch.rand([B,M], dtype=torch.float)  
+    x_0 = torch.zeros((B, H**2), dtype=torch.float)
+    var = torch.zeros((B, M), dtype=torch.float)
+
+    x = recon(y, x_0, var, meas)
+    print(x.shape)
+    assert_test(x.shape, torch.Size([85, 1024]), "Wrong recon size")
+
+    #%% DCNet
+    from spyrit.core.recon import DCNet
+
+    B, C, H, M = 10, 1, 64, 64**2//2
+
+    # constructor
+    Ord = np.ones((H,H))
+    meas = HadamSplit(M, H, Ord)
+    noise = NoNoise(meas)
+    prep = SplitPoisson(1.0, M, H**2)
+    sigma = np.random.random([H**2, H**2])
+    recnet = DCNet(noise,prep,sigma)
+
+    # forward
+    x = torch.FloatTensor(B,C,H,H).uniform_(-1, 1)
+    z = recnet(x)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([10, 1, 64, 64]), "Wrong recon size")
+
+    # reconstruct
+    x = torch.rand((B*C,2*M), dtype=torch.float)
+    z = recnet.reconstruct(x)
+    print(z.shape)
+    assert_test(z.shape, torch.Size([10, 1, 64, 64]), "Wrong recon size")
+
+if __name__ == '__main__':
+    test_core_recon()
