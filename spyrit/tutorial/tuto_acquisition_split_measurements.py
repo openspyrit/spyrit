844dc68c1a59ddb1cc953ba5e7e59a245510be41
jabascal <juanabascal78@gmail.com>
2023-08-21 13:23:35 +0200

    Tutorial on Hadamard split measurements

---
+++ spyrit/tutorial/tuto_acquisition_split_measurements.py
@@ -0,0 +1,253 @@
+
+r"""
+.. _tuto_acquisition_split_measurements:
+05. Acquisition operators 2 - Split measurements
+==========================
+This tutorial is a continuation of :ref:`tuto_acquisition_operators` for single-pixel imaging. 
+The previous tutorial showed how to simulate linear measurements using the :class:`spyrit.core` submodule 
+(based on three classes :class:`spyrit.core.meas, :class:`spyrit.core.noise`, and :class:`spyrit.core.prep`). 
+This tutorial shows how to simulate linear measurements from Hadamard patterns and 
+split measurements into two parts, in order to handle negative measurements. 
+
+In the next sections, we show how to simulate split measurements and how to reconstruct images from them.         
+
+"""
+
+import numpy as np
+import os
+from spyrit.misc.disp import imagesc
+import matplotlib.pyplot as plt
+
+
+# %%
+# Load a batch of images
+#-----------------------
+
+###############################################################################
+# Images :math:`x` for training neural networks expect values in [-1,1]. The images are normalized
+# using the :func:`transform_gray_norm` function.
+
+from spyrit.misc.statistics import transform_gray_norm
+import torchvision
+import torch
+
+h = 64            # image size hxh 
+i = 1             # Image index (modify to change the image) 
+spyritPath = os.getcwd()
+imgs_path = os.path.join(spyritPath, '../images')
+
+
+# Create a transform for natural images to normalized grayscale image tensors
+transform = transform_gray_norm(img_size=h)
+
+# Create dataset and loader (expects class folder 'images/test/')
+dataset = torchvision.datasets.ImageFolder(root=imgs_path, transform=transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = 7)
+
+x, _ = next(iter(dataloader))
+print(f'Shape of input images: {x.shape}')
+
+# Select image
+x = x[i:i+1,:,:,:]
+x = x.detach().clone()
+b,c,h,w = x.shape
+
+# plot
+x_plot = x.view(-1,h,h).cpu().numpy() 
+imagesc(x_plot[0,:,:], r'$x$ in [-1, 1]')
+
+# %%
+# The measurement and noise operators
+#-------------------
+
+############################################################################### 
+# Noise operators are defined in the :mod:`~spyrit.core.noise` module. A noise
+# operator computes the following three steps sequentially: 
+#   1. Normalization of the image :math:`x` with values in [-1,1] to get an 
+#      image :math:`\tilde{x}=\frac{x+1}{2}` in [0,1], as it is required for measurement simulation
+#   2. Application of the measurement model, i.e., computation of :math:`P\tilde{x}`
+#   3. Application of the noise model
+# 
+# .. math::
+#       y \sim \texttt{Noise}(P\tilde{x}) = \texttt{Noise}\left(\frac{P(x+1)}{2}\right),
+#
+# The normalization is usefull when considering distributions such
+# as the Poisson distribution that are defined on positive values. 
+
+###############################################################################
+# .. note::
+#   Note that the image identical to the original one, except it has been 
+#   normalized in [0,1].
+
+# %% 
+# Split measurement operator and no noise
+#-----------------------------------------------------------
+
+###############################################################################
+# .. math::
+#       y = P\tilde{x}= \begin{bmatrix} H_{+} \\ H_{-} \end{bmatrix} \tilde{x}.
+
+###############################################################################
+# Hadamard split measurement operator is defined in the :class:`spyrit.core.meas.HadamSplit` class. 
+# It computes linear measurements from incoming images, where :math:`P` is a 
+# linear operator (matrix) with positive entries and :math:`\tilde{x}` is a vectorized image. 
+# The class relies on a matrix :math:`H` with 
+# shape :math:`(M,N)` where :math:`N` represents the number of pixels in the 
+# image and :math:`M \le N` the number of measurements. The matrix :math:`P` 
+# is obtained by splitting the matrix :math:`H` such that 
+# :math:`P = \begin{bmatrix}{H_{+}}\\{H_{-}}\end{bmatrix}`, where 
+# :math:`H_{+} = \max(0,H)`, :math:`H_{-} = \max(0,-H)`, and 
+# :math:`H = H_{+}-H_{-}`.`
+
+###############################################################################
+# The matrix :math:`H` is obtained by retaining the first :math:`M` rows of 
+# a permuted Hadamard matrix :math:`Perm H`, where :math:`Perm` is a 
+# permutation matrix with shape with shape :math:`(M,N)` and :math:`H` is a 
+# "full" Hadamard matrix with shape :math:`(N,N)` (see :ref:`tuto_pseudoinverse_linear`).
+# The permutation matrix :math:`Perm` is obtained by permuting the rows of :math:`H`, 
+# as indicated by the order matrix :math:`Ord` with shape :math:`(h,h)`. 
+# 
+# Then, we simulate an accelerated acquisition by subsampling the measurement matrix 
+# by preserving only the first :math:`M` rows.
+
+###############################################################################
+# .. note::
+#   Note that the positive component of a Hadamard matrix has been used in  
+# previous tutorials (see :ref:`tuto_pseudoinverse_linear`). In that case, 
+# we could proceed as with a general linear operator. 
+
+# We consider the noiseless case handled 
+# by the :class:`spyrit.core.noise.NoNoise` class.
+M = 64*64 // 4       # number of measurements (here, 1/4 of the pixels)
+
+from spyrit.core.meas import HadamSplit   
+from spyrit.core.noise import NoNoise
+from spyrit.misc.sampling import meas2img2
+
+Ord = np.ones((h,h))                
+meas_op = HadamSplit(M, h, Ord)
+nonoise_op = NoNoise(meas_op) # noiseless
+
+###############################################################################
+# We simulate the measurement vector :math:`y` that we visualise as an image. 
+# Remember that the input image :math:`x` is handled as a vector.  
+x = x.view(b*c,h*w)  # vectorized image
+print(f'Shape of vectorized image: {x.shape}')
+y_noiseless = nonoise_op(x)  # noiseless measurement vector
+print(f'Shape of simulated measurements y: {y_noiseless.shape}')
+
+# Plot
+y_plot = y_noiseless.numpy()   
+y_plot = meas2img2(y_plot.T, Ord)
+y_plot = np.moveaxis(y_plot,-1, 0)
+imagesc(y_plot[0,:,:], r'Noiseless measurements $y$')
+
+# %% 
+# Same example with Poisson noise
+#--------------------------------
+
+###############################################################################
+# We now consider Poisson noise, i.e., a noisy measurement vector given by
+#
+# .. math::
+#       y \sim \mathcal{P}(\alpha P \tilde{x}),
+#
+# where :math:`\alpha` is a scalar value that represents the maximum image intensity
+# (in photons). The larger :math:`\alpha`, the higher the signal-to-noise ratio.
+#
+
+###############################################################################
+# We consider the :class:`spyrit.core.noise.Poisson` class and set :math:`\alpha`
+# to 100 photons. We simulate a noisy measurement vector. 
+
+from spyrit.core.noise import Poisson
+from spyrit.misc.disp import add_colorbar, noaxis
+
+alpha = 100.0 # number of photons
+noise_op = Poisson(meas_op, alpha) 
+y_noisy = noise_op(x) # a noisy measurement vector
+
+# Plot
+y_plot2 = y_noisy.numpy()   
+y_plot2 = meas2img2(y_plot2.T, Ord)
+y_plot2 = np.moveaxis(y_plot2,-1, 0)
+imagesc(y_plot2[0,:,:], r'Noisy measurements $y$ ($N=100$ photons)')
+
+
+# %% 
+# The preprocessing operator
+#---------------------------
+
+###############################################################################
+# Preprocessing operators are defined in the :mod:`spyrit.core.prep` module. 
+# A preprocessing operator applies to the noisy measurements 
+# 
+# .. math::
+#       m = \texttt{Prep}(y),
+# 
+# For instance, a preprocessing operator can be used to compensate for the 
+# scaling factors that appear in the measurement or noise operators. In this 
+# case, a preprocessing operator is closely linked to its measurement and/or 
+# noise operator counterpart. While scaling factors are required to simulate 
+# realistic measurements, they are not required for reconstruction.
+
+# %% 
+# Preprocessing measurements corrupted by Poisson noise
+#------------------------------------------------------
+
+###############################################################################
+# We consider the :class:`spyrit.core.prep.SplitPoisson` class that intends 
+# to "undo" the :class:`spyrit.core.noise.Poisson` class, for split measurements, by compensating for
+#   * the scaling that appears when computing Poisson-corrupted measurements
+#   * the affine transformation to get images in [0,1] from images in [-1,1]
+#
+# For this, it computes
+#
+# .. math::
+#       m = \frac{2}{\alpha} y - P1
+#     
+
+###############################################################################
+# We consider first preprocessing the measurements corrupted by Poisson noise
+from spyrit.core.prep import SplitPoisson
+
+alpha = 100.0 # number of photons
+prep_op_noisy = SplitPoisson(alpha, meas_op) 
+m_noisy = prep_op_noisy(y_noisy) 
+
+# plot
+m_plot = m_noisy.numpy()   
+m_plot = meas2img2(m_plot.T, Ord)
+m_plot = np.moveaxis(m_plot,-1, 0)
+imagesc(m_plot[0,:,:], r'Noiseless measurements $m$')
+
+###############################################################################
+# Similarly, we can preprocess the noiseless measurement by setting :math:`\alpha` to 1.
+prep_op_noiseless = SplitPoisson(1.0, meas_op) 
+m_noiseless = prep_op_noiseless(y_noiseless)
+
+# Plot
+m_plot2 = m_noiseless.numpy()   
+m_plot2 = meas2img2(m_plot2.T, Ord)
+m_plot2 = np.moveaxis(m_plot2,-1, 0)
+imagesc(m_plot2[0,:,:], r'Noisy measurements $m$ (N=100 photons)')
+
+###############################################################################
+#
+# In the next tutorial, we will show how to reconstruct images from split measurements.
+
+# %%
+# Check reconstructions
+from spyrit.core.recon import PseudoInverse
+
+recon_op = PseudoInverse()
+z_noiseless = recon_op(m_noiseless, meas_op)
+z_noisy = recon_op(m_noisy, meas_op)
+
+# Plot
+x_plot = x.view(-1,h,h).numpy() 
+imagesc(x_plot[0,:,:],'Ground-truth image')
+z_plot = z_noiseless.view(-1,h,h).numpy() 
+imagesc(z_plot[0,:,:],'Reconstructed noiseless')
+z_plot = z_noisy.view(-1,h,h).numpy() 
+imagesc(z_plot[0,:,:],'Reconstructed noisy')
