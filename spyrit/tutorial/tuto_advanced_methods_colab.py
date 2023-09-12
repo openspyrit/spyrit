8c83e7b566efcff65c85dc8b8b9ac9a1bd9eaf77
jabascal <juanabascal78@gmail.com>
2023-09-12 11:32:59 +0200

    Create tuto to link to spyrit-examples

---
+++ spyrit/tutorial/tuto_advanced_methods_colab.py
@@ -0,0 +1,12 @@
+r"""
+07. Advanced methods - Colab
+==========================
+.. _tuto_advanced_methods_colab:
+We refer to `spyrit-examples/tutorial <https://github.com/openspyrit/spyrit-examples/tree/master/tutorial>`_ 
+for a list of tutorials that can be run directly in colab and present more advanced cases than the main spyrit tutorials, 
+such as comparison of methods for split measurements, or comparison of different denoising networks.     
+
+"""
+
+###############################################################################
+# The spyrit-examples repository also includes research contributions based on the SPYRIT toolbox.
