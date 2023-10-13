9559b150b1e8d828514aa835cc3c3854b5c6287d
jabascal <juanabascal78@gmail.com>
2023-10-13 10:11:39 +0200

    UPGD modifs: work in  progress

--- spyrit/core/recon.py
+++ spyrit/core/recon.py
@@ -896,10 +896,10 @@ class DCDRUNet(DCNet):
 #%%===========================================================================================
 class PositiveParameters(nn.Module):
 # ===========================================================================================
-    def __init__(self, size, val_min=1e-8):
+    def __init__(self, size, val=1e-8):
         super(PositiveParameters, self).__init__()
-        self.val_min = torch.tensor(val_min)
-        self.params = nn.Parameter(torch.abs(val_min*torch.ones(size,1)), requires_grad=True)
+        self.val_min = torch.tensor(val)
+        self.params = nn.Parameter(torch.abs(val*torch.ones(size,1)), requires_grad=True)
         
     def forward(self):
         return torch.abs(self.params)
@@ -914,96 +914,64 @@ class PositiveMonoIncreaseParameters(PositiveParameters):
         # cumsum in opposite order
         return super().forward().cumsum(dim=0).flip(dims=[0])
 
-#%%===========================================================================================
-class UPGD(PinvNet):
-# ===========================================================================================
+class UPGD(nn.Module):
     def __init__(self, 
-                 noise, 
-                 prep, 
+                 noise,
+                 prep,
                  denoi=nn.Identity(),
-                 num_iter = 6, 
-                 lamb = 1e-5, 
-                 lamb_min = 1e-10, 
-                 split=False):
-        super(UPGD, self).__init__(noise, prep, denoi)
+                 num_iter = 6,
+                 lamb = 1e-5,
+                 *args, 
+                 **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.num_iter = num_iter
-        self.lamb = lamb
-        self.lamb_min = lamb_min
-        # Set a trainable tensor for the regularization parameter with dimension num_iter
-        # and constrained to be positive with clamp(min=0.0, max=None)        
-        self.lambs = PositiveParameters(num_iter, lamb_min) # shape lambs = [num_iter,1]
-        #self.lambs = nn.Parameter(torch.abs(lamb_min*torch.ones(num_iter,1)), requires_grad=True)
-        #self.lambs = nn.Parameter(torch.abs(lamb*torch.ones(num_iter,1)), requires_grad=False)
-        #self.noise = noise
-        self.split = split
+        #self.lamb = lamb
+        self.lambs = PositiveParameters(num_iter, lamb) # shape lambs = [num_iter,1]
+        #self.lambs = PositiveMonoIncreaseParameters(num_iter, lamb) # shape lambs = [num_iter,1]
+        #self.lambs = nn.Parameter(torch.abs(lamb*torch.ones(num_iter,1)), requires_grad=True)
+        self.acqu = noise
+        self.prep = prep
+        self.denoi = denoi
+    
+    def forward(self, x):
+        
+        b,c,_,_ = x.shape
+
+        # Acquisition
+        x = x.view(b*c,self.acqu.meas_op.N)  # shape x = [b*c,h*w] = [b*c,N]
+        x = self.acqu(x)                     # shape x = [b*c, 2*M]
+
+        # Reconstruction
+        x = self.reconstruct(x)             # shape x = [bc, 1, h,w]
+        x = x.view(b,c,self.acqu.meas_op.h, self.acqu.meas_op.w)
+        return x
+    
+    def acquire(self, x):
+        b,c,_,_ = x.shape
+
+        # Acquisition
+        x = x.view(b*c,self.acqu.meas_op.N)  # shape x = [b*c,h*w] = [b*c,N]
+        x = self.acqu(x)                     # shape x = [b*c, 2*M]
+        return x
 
     def reconstruct(self, x):
-        r""" Reconstruction step§ of a reconstruction network
-        
-        Same as :meth:`reconstruct` reconstruct except that:
-            
-            1. The regularization parameter is trainable
-            
-        Args:
-            :attr:`x`: raw measurement vectors
-        
-        Shape:
-            :attr:`x`: :math:`(BC,2M)`
-            
-            :attr:`output`: :math:`(BC,1,H,W)`
-        """
-
-        # Measurement operator
-        # if self.split:
-        #     meas_op = super().Acq.meas_op     
-        #     fwd_op = meas_op.forward_H
-        #     adj_op = meas_op.forward_H_adjoint
-        # else:            
-        meas_op = self.acqu.meas_op
-        fwd_op = meas_op.H
-        adj_op = meas_op.H_adjoint
-
         # x of shape [b*c, 2M]
-        bc, _ = x.shape    
+        bc, _ = x.shape
+    
+        # Preprocessing
+        m = self.prep(x)
 
-        # Save measurements
-        y = x.clone() # [5, 1024]
+        # First solution: Pseudo inverse
+        x = self.acqu.meas_op.pinv(m)  # shape x = [b*c,N]
 
-        # First estimate: Pseudo inverse
-        # Preprocessing in the measurement domain
-        x = self.prep(x) # [5, 1024]
-
-        # Save prep measurements
-        m = x.clone() # [5, 1024]
-
-        # Pinv
-        x = self.pinv(x, meas_op)  # [5, 4096]         # shape x = [b*c,N]
-        x = super(UPGD, self).reconstruct(y)
-
-        # GD step
-        #x = self.lambs()[0]*adj_op(m)
-        #x = x.view(bc,1,self.acqu.meas_op.h, self.acqu.meas_op.w)   # shape x = [b*c,1,h,w]
-        
-        # Denoising step
-        x = x.view(bc,1,meas_op.h, meas_op.w) # [5, 1, 64, 64]
-        x = self.denoi(x)
-        x = x.view(bc, meas_op.N) # [5, 4096]
-
-        # Unroll network
-        # Ensure step size is positive and monotonically decreasing and larger than self.lamb!
-        lambs = self.lambs()
-        #lambs = self.lambs
+        # Loop over the number of iterations
         for n in range(self.num_iter):
-
-            # Residual
-            res = fwd_op(x) - m # [5, 1024]
-
             # Gradient step
-            x = x - lambs[n]*adj_op(res) # [5, 4096]
+            res = self.acqu.meas_op.H(x) - m
+            x = x - self.lambs()[n]*self.acqu.meas_op.H_adjoint(res)
+            x = x.view(bc,1,self.acqu.meas_op.h, self.acqu.meas_op.w)   # shape x = [b,1,h,w]
 
             # Denoising step
-            x = x.view(bc,1,meas_op.h, meas_op.w) # [5, 1, 64, 64]
             x = self.denoi(x)
-            x = x.view(bc, meas_op.N) # [5, 4096]
-        return x            
-    
+            x = x.view(bc,self.acqu.meas_op.h*self.acqu.meas_op.w)   # shape x = [b,1,h,w]
+        return x
