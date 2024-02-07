b019f0c512e1cf3c8ba76e6663891a3c2c05fa71
romainphan <romainphan.rp@gmail.com>
2024-02-07 16:33:04 +0100

    Documentation update

---
+++ spyrit/test/test_core_time.py
@@ -0,0 +1,17 @@
+"""
+Test for module time.py
+Author: Romain Phan
+"""
+
+import torch
+
+from spyrit.core.time import DeformationField, AffineDeformationField
+from test_helpers import assert_test
+
+
+def test_core_time():
+    
+    ## Test DeformationField
+    # test
+    
+    return True
