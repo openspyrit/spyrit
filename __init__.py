a99e01f53491a4b2d773858333781c710b102d25
romainphan <romainphan.rp@gmail.com>
2024-02-16 11:33:39 +0100

    deleted spyrit/__init__

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# from __future__ import division, print_function, absolute_import
-# from distutils.version import LooseVersion
-#
-#
-# from . import spyritest
-#
-#
-# __all__ = [s for s in dir() if not s.startswith('_')]
