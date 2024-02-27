b07f8f042e63398a94dd9537ce8c77aa8af211b1
romainphan <romainphan.rp@gmail.com>
2024-02-27 10:27:29 +0100

    corrected image path in tutos

--- tutorial/tuto_06_dcnet_split_measurements.py
+++ tutorial/tuto_06_dcnet_split_measurements.py
@@ -177,7 +177,7 @@ with torch.no_grad():
 # Only the last step involves learnable parameters.
 
 ###############################################################################
-# .. image:: /spyrit/docs/source/fig/dcnet.png
+# .. image:: ../fig/dcnet.png
 #    :width: 400
 #    :align: center
 #    :alt: Sketch of the DCNet architecture
