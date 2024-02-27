0e047ce7b43ae14973cb4a15e09fe978ac3cac82
romainphan <romainphan.rp@gmail.com>
2024-02-27 11:44:37 +0100

    trying to use new thumbnail size

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,7 +96,7 @@ sphinx_gallery_conf = {
 html_theme = "sphinx_rtd_theme"
 
 # directory containing custom CSS file (used to produce bigger thumbnails)
-html_static_path = ["_static"]
+html_static_path = ["_static/thumbnail.css"]
 
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
