d2379e74b65f0e188cc04d766cb318a6f6bc9d30
romainphan <romainphan.rp@gmail.com>
2024-02-28 15:16:38 +0100

    updating existing tests

--- spyrit/test/test_helpers.py
+++ spyrit/test/test_helpers.py
@@ -6,3 +6,9 @@ def assert_test(condition1, condition2, message):
         print("ERROR: " + str(condition1) + " is not equal to " + str(condition2))
         print(message)
         sys.exit(-1)
+
+def assert_elementwise_equal(elem1, elem2, message):
+    if not (elem1 == elem2).all():
+        print("ERROR: " + str(elem1) + " is not equal to " + str(elem2))
+        print(message)
+        sys.exit(-1)
