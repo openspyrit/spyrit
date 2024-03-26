ab88ef5779c434ee966328f2073b75b91f381dc3
romainphan <romainphan.rp@gmail.com>
2024-03-26 15:58:32 +0100

    display change for test_helpers

--- spyrit/test/test_helpers.py
+++ spyrit/test/test_helpers.py
@@ -3,13 +3,14 @@ import sys
 
 def assert_test(condition1, condition2, message):
     if not (condition1 == condition2):
-        print("ERROR: " + str(condition1) + " is not equal to " + str(condition2))
+        print("\n" + "ERROR: " + str(condition1) + " is not equal to " + str(condition2))
         print(message)
         sys.exit(-1)
 
 
 def assert_elementwise_equal(elem1, elem2, message):
     if not (elem1 == elem2).all():
-        print("ERROR: " + str(elem1) + " is not equal to " + str(elem2))
+        print("\n" + "_" * 80)
+        print("ERROR: " + str(elem1) + "\nis not equal to\n" + str(elem2))
         print(message)
         sys.exit(-1)
