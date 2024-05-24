e0fb62289b5124fc27d3456f00851771a9c0fa1b
nducros <nicolas.ducros@insa-lyon.fr>
2024-05-24 19:42:18 +0200

    Same construction of the sampling matrix (undo)

--- tutorial/tuto_05_acquisition_split_measurements.py
+++ tutorial/tuto_05_acquisition_split_measurements.py
@@ -168,11 +168,11 @@ Ord_var = Cov2Var(Cov)
 # sphinx_gallery_thumbnail_number = 2
 
 # Mask for "naive subsampling"
-# mask_nai = np.zeros((h, h))
-# mask_nai[0 : int(M / h), :] = 1
-idx = np.argsort(Ord_nai.ravel(), axis=None)[::-1]
-mask_nai = np.zeros_like(Ord_nai)
-mask_nai.flat[idx[0:M]] = 1
+mask_nai = np.zeros((h, h))
+mask_nai[0 : int(M / h), :] = 1
+#idx = np.argsort(Ord_nai.ravel(), axis=None)[::-1]
+#mask_nai = np.zeros_like(Ord_nai)
+#mask_nai.flat[idx[0:M]] = 1
 
 # Mask for "variance subsampling"
 idx = np.argsort(Ord_var.ravel(), axis=None)[::-1]
