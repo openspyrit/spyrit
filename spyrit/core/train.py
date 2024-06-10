43288c16f4282c1b34b72f13f7f4bf84aa64a57e
romainphan <romainphan.rp@gmail.com>
2024-06-10 14:44:36 +0200

    corrected .pth.pth double extension

--- spyrit/core/train.py
+++ spyrit/core/train.py
@@ -479,7 +479,7 @@ class Train_par:
 
     def plot(self, start=0):
         plt.ion()
-        string1 = "Batch Size : \t {} \ Learning : \t {} \n".format(
+        string1 = "Batch Size : \t {} \n Learning : \t {} \n".format(
             self.batch_size, self.learning_rate
         )
         string2 = "size : \t {} \nRegularisation : \t {}".format(
@@ -811,7 +811,7 @@ def load_net(title, model, device=None, strict=True):
     # if title.endswith(".pth"):
     #     model_out_path = "{}".format(title)
     # else:
-    model_out_path = "{}.pth".format(title)
+    model_out_path = title
     try:
         if device is None:
             model.load_state_dict(torch.load(model_out_path), strict=strict)
