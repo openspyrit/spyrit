d105b63f78de0715c0b17d9586aec17af7a5eb67
romainphan <romainphan.rp@gmail.com>
2024-06-10 17:29:34 +0200

    tuto8 pdf debugging

--- tutorial/tuto_08_lpgd_split_measurements.py
+++ tutorial/tuto_08_lpgd_split_measurements.py
@@ -71,3 +71,53 @@ b, c, h, w = x.shape
 # plot
 x_plot = x.view(-1, h, h).cpu().numpy()
 imagesc(x_plot[0, :, :], r"$x$ in [-1, 1]")
+
+# %%
+# Forward operators for split measurements
+# -----------------------------------------------------------------------------
+
+###############################################################################
+# We consider noisy split measurements for a Hadamard operator and a simple
+# rectangular subsampling” strategy
+# (for more details, refer to :ref:`Acquisition - split measurements <tuto_acquisition_split_measurements>`).
+#
+#
+# We define the measurement, noise and preprocessing operators and then
+# simulate a measurement vector :math:`y` corrupted by Poisson noise. As in the previous tutorial,
+# we simulate an accelerated acquisition by subsampling the measurement matrix
+# by retaining only the first rows of a Hadamard matrix.
+
+from spyrit.core.meas import HadamSplit
+from spyrit.core.noise import Poisson
+from spyrit.misc.sampling import meas2img2
+from spyrit.misc.statistics import Cov2Var
+from spyrit.core.prep import SplitPoisson
+
+import math
+
+# Measurement parameters
+M = 4096  # Number of measurements (here, 1/4 of the pixels)
+alpha = 10.0  # number of photons
+
+# Sampling: rectangular matrix
+Ord_rec = np.ones((h, h))
+n_sub = math.ceil(M**0.5)
+Ord_rec[:, n_sub:] = 0
+Ord_rec[n_sub:, :] = 0
+
+# Measurement and noise operators
+meas_op = HadamSplit(M, h, Ord_rec)
+noise_op = Poisson(meas_op, alpha)
+prep_op = SplitPoisson(alpha, meas_op)
+
+# Vectorize image
+x = x.view(b * c, h * w)
+print(f"Shape of vectorized image: {x.shape}")
+
+# Measurements
+y = noise_op(x)  # a noisy measurement vector
+m = prep_op(y)  # preprocessed measurement vector
+
+m_plot = m.detach().numpy()
+m_plot = meas2img2(m_plot.T, Ord_rec)
+imagesc(m_plot, r"Measurements $m$")
