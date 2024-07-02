e0d5a2524d48f2539e15814a804749418c7648f4
romainphan <romainphan.rp@gmail.com>
2024-07-02 13:53:20 +0200

    version number updates in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Antonio Tomas Lorente Mur - Nicolas Ducros - Sebastien Crombe
 author = "Antonio Tomas Lorente Mur - Nicolas Ducros - Sebastien Crombez - Thomas Baudier - Romain Phan"
 
 # The full version, including alpha/beta/rc tags
-release = "2.1.0"
+release = "2.3.2"
 
 # -- General configuration ---------------------------------------------------
 
