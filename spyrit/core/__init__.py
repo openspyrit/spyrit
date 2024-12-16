d4b44db534757f6da75de436c108071dd5d4d14a
romainphan <romainphan.rp@gmail.com>
2024-12-16 15:29:20 +0100

    added imports to __init__ files

--- spyrit/core/__init__.py
+++ spyrit/core/__init__.py
@@ -1,2 +1,9 @@
 """Core module for Spyrit package, containing the main classes and functions.
 """
+
+from . import meas
+from . import noise
+from . import prep
+from . import recon
+from . import nnet
+from . import train
