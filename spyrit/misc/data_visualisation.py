d3f4d49d6cdddafe8f4500fccc6ff0262ed524cb
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2025-12-17 18:31:24 +0100

    Remove astropy dependency

--- spyrit/misc/data_visualisation.py
+++ spyrit/misc/data_visualisation.py
@@ -12,30 +12,7 @@ Created on Thu Jan 16 08:56:13 2020
 @author: crombez
 """
 
-from astropy.io import fits
 import matplotlib.pyplot as plt
-
-
-# Show basic information of a fits image acquired
-# with the andor zyla and plot the image
-def show_image_and_infos(path, file):
-    hdul = fits.open(path + file)
-    show_images_infos(path, file)
-    plt.figure()
-    plt.imshow(hdul[0].data[0])
-    plt.show()
-
-
-def show_images_infos(path, file):  # Show basic information of a fits image acquired
-    hdul = fits.open(path + file)
-    print("***** Name file : " + file + " *****")
-    print("Type de données : " + hdul[0].header["DATATYPE"])
-    print("Mode d'acquisition : " + hdul[0].header["ACQMODE"])
-    print("Temps d'exposition : " + str(hdul[0].header["EXPOSURE"]))
-    print("Temps de lecture : " + str(hdul[0].header["READTIME"]))
-    print("Longeur d'onde de Rayleigh : " + str(hdul[0].header["RAYWAVE"]))
-    print("Longeur d'onde détectée : " + str(hdul[0].header["DTNWLGTH"]))
-    print("***********************************" + "\n")
 
 
 # Plot the resulting fuction of to set of 1D data with the same dimension
